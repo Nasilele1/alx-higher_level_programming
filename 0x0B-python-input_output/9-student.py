5432cebbfe1a970b2f338f5a86f934f45e13c834
Nasilele1 <jasperkatondo@gmail.com>
2023-04-26 03:17:00 -0700

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,7 +5,7 @@
 class Student:
     """Represent a student."""
 
-    def ___init__(self, first_name, last_name, age):
+    def __init__(self, first_name, last_name, age):
         """Initialise a new student.
 
         Args:
