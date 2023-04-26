e7a52b8a5ec07672fb20b592e9d9695cfa4f1753
Nasilele1 <jasperkatondo@gmail.com>
2023-04-26 03:42:16 -0700

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines a student class"""
+
+
+class student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialise a new student.
+
+        Args:
+            first_name (str): The first name of a student.
+            last_name (str): The last name of a student.
+            age (int): The age of a student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the student.
+
+        If attrs is a list of strings, represent only those attributes
+        included in the list
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
